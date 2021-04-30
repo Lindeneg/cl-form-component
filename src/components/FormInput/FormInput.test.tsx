79f2d5ea65801c5492761164edfce9bea6147375
Christian <hi@lindeneg.org>
2021-04-30 06:12:57 +0200

    check for valid and invalid styles

--- src/components/FormInput/FormInput.test.tsx
+++ src/components/FormInput/FormInput.test.tsx
@@ -9,7 +9,6 @@ describe('Input', () => {
 
         expect(target).toBeInTheDocument();
         expect(target).toHaveStyle({
-            outlineColor: 'rgb(0, 123, 255)',
             backgroundColor: 'rgb(255, 255, 255)',
             color: 'rgb(52, 58, 64)'
         });
@@ -25,20 +24,28 @@ describe('Input', () => {
         expect(target).toHaveAttribute('placeholder', 'write something');
     });
     test('renders a valid input field', async () => {
-        render(<FormInput isValid id="test-id" validFeedback="Text is valid" />);
+        const dom = render(<FormInput isValid id="test-id" validFeedback="Text is valid" />);
+        const target = queryByAttribute('id', dom.container, 'test-id');
 
         expect(screen.getByText('Text is valid')).toBeInTheDocument();
         expect(screen.getByText('Text is valid')).toHaveStyle({
             color: 'rgb(40, 167, 69)'
         });
+        expect(target).toHaveStyle({
+            border: '1px solid #28a745'
+        });
     });
 
     test('renders an invalid input field', async () => {
-        render(<FormInput isInvalid id="test-id" invalidFeedback="Text is invalid" />);
+        const dom = render(<FormInput isInvalid id="test-id" invalidFeedback="Text is invalid" />);
+        const target = queryByAttribute('id', dom.container, 'test-id');
 
         expect(screen.getByText('Text is invalid')).toBeInTheDocument();
         expect(screen.getByText('Text is invalid')).toHaveStyle({
             color: 'rgb(235, 87, 101)'
+        });
+        expect(target).toHaveStyle({
+            border: '1px solid #eb5765'
         });
     });
 });
