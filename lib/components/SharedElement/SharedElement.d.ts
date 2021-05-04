0ce4513f72c50f951eac7d099c6635fcd932f850
Christian <hi@lindeneg.org>
2021-05-04 06:26:10 +0200

    clena lib

--- lib/components/SharedElement/SharedElement.d.ts
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-import { SharedInputProps } from './shared.props';
-interface SharedElementProps extends SharedInputProps {
-    center?: boolean;
-}
-declare const SharedElement: FC<SharedElementProps>;
-export default SharedElement;
