144f98e9ac09c87f9a4344b23c91b449f94e18cc
Christian <hi@lindeneg.org>
2021-05-04 04:46:36 +0200

    prettier

--- src/components/FormSelect/FormSelect.test.tsx
+++ src/components/FormSelect/FormSelect.test.tsx
@@ -3,47 +3,47 @@ import { render, fireEvent, queryByAttribute } from '@testing-library/react';
 import FormSelect from './FormSelect';
 
 describe('Select', () => {
-	test('renders a default select field', async () => {
-		const dom = render(<FormSelect id="test-id" />);
-		const target = queryByAttribute('id', dom.container, 'test-id');
+    test('renders a default select field', async () => {
+        const dom = render(<FormSelect id="test-id" />);
+        const target = queryByAttribute('id', dom.container, 'test-id');
 
-		expect(target).toBeInTheDocument();
-		expect(target).toHaveStyle({
-			backgroundColor: 'rgb(255, 255, 255)',
-			color: 'rgb(52, 58, 64)'
-		});
-	});
-	test('renders a default dark select field', async () => {
-		const dom = render(<FormSelect id="test-id" variant="dark" />);
-		const target = queryByAttribute('id', dom.container, 'test-id');
+        expect(target).toBeInTheDocument();
+        expect(target).toHaveStyle({
+            backgroundColor: 'rgb(255, 255, 255)',
+            color: 'rgb(52, 58, 64)'
+        });
+    });
+    test('renders a default dark select field', async () => {
+        const dom = render(<FormSelect id="test-id" variant="dark" />);
+        const target = queryByAttribute('id', dom.container, 'test-id');
 
-		expect(target).toBeInTheDocument();
-		expect(target).toHaveStyle({
-			backgroundColor: 'rgb(52, 58, 64)',
-			color: 'rgb(255, 255, 255)'
-		});
-	});
-	test('renders a select field with entries', async () => {
-		const dom = render(<FormSelect id="test-id" selectOptions={['Hello', 'There']} />);
-		const target = queryByAttribute('id', dom.container, 'test-id');
+        expect(target).toBeInTheDocument();
+        expect(target).toHaveStyle({
+            backgroundColor: 'rgb(52, 58, 64)',
+            color: 'rgb(255, 255, 255)'
+        });
+    });
+    test('renders a select field with entries', async () => {
+        const dom = render(<FormSelect id="test-id" selectOptions={['Hello', 'There']} />);
+        const target = queryByAttribute('id', dom.container, 'test-id');
 
-		expect(target?.children.length).toBe(2);
-		expect(target).toHaveValue('Hello');
-	});
-	test('handles select on click', async () => {
-		const mockOnClick = jest.fn();
-		const dom = render(<FormSelect id="test-id" onClick={mockOnClick} />);
-		const target = queryByAttribute('id', dom.container, 'test-id');
-		target && fireEvent.click(target);
+        expect(target?.children.length).toBe(2);
+        expect(target).toHaveValue('Hello');
+    });
+    test('handles select on click', async () => {
+        const mockOnClick = jest.fn();
+        const dom = render(<FormSelect id="test-id" onClick={mockOnClick} />);
+        const target = queryByAttribute('id', dom.container, 'test-id');
+        target && fireEvent.click(target);
 
-		expect(mockOnClick).toHaveBeenCalledTimes(1);
-	});
-	test('handles select on change', async () => {
-		const mockOnClick = jest.fn();
-		const dom = render(<FormSelect id="test-id" onChange={mockOnClick} />);
-		const target = queryByAttribute('id', dom.container, 'test-id');
-		target && fireEvent.change(target);
+        expect(mockOnClick).toHaveBeenCalledTimes(1);
+    });
+    test('handles select on change', async () => {
+        const mockOnClick = jest.fn();
+        const dom = render(<FormSelect id="test-id" onChange={mockOnClick} />);
+        const target = queryByAttribute('id', dom.container, 'test-id');
+        target && fireEvent.change(target);
 
-		expect(mockOnClick).toHaveBeenCalledTimes(1);
-	});
+        expect(mockOnClick).toHaveBeenCalledTimes(1);
+    });
 });
--- src/components/FormSelect/FormSelect.tsx
+++ src/components/FormSelect/FormSelect.tsx
@@ -4,78 +4,78 @@ import SharedElement, { SharedInputProps } from '../SharedElement';
 import { getVariantCSS, capitalize } from '../../util';
 
 const selectStyles: CSSProperties = {
-	boxSizing: 'border-box',
-	border: 'none',
-	padding: '0 1rem 0 0',
-	margin: '0',
-	outline: 'none',
-	width: '100%',
-	fontFamily: 'inherit',
-	fontSize: 'inherit',
-	cursor: 'inherit',
-	lineHeight: 'inherit'
+    boxSizing: 'border-box',
+    border: 'none',
+    padding: '0 1rem 0 0',
+    margin: '0',
+    outline: 'none',
+    width: '100%',
+    fontFamily: 'inherit',
+    fontSize: 'inherit',
+    cursor: 'inherit',
+    lineHeight: 'inherit'
 };
 
 const selectDiv: CSSProperties = {
-	width: '100%',
-	minWidth: '15ch',
-	maxWidth: '30ch',
-	border: '1px solid #777',
-	borderRadius: '.25rem',
-	padding: '.25rem .5rem',
-	fontSize: '1.1rem',
-	cursor: 'pointer',
-	lineHeight: '1.5rem'
+    width: '100%',
+    minWidth: '15ch',
+    maxWidth: '30ch',
+    border: '1px solid #777',
+    borderRadius: '.25rem',
+    padding: '.25rem .5rem',
+    fontSize: '1.1rem',
+    cursor: 'pointer',
+    lineHeight: '1.5rem'
 };
 
 export type FormSelectOptionProps = {
-	value: string;
-	displayValue?: string;
-	selected?: boolean;
+    value: string;
+    displayValue?: string;
+    selected?: boolean;
 };
 
 export type FormSelectMetaProps = {
-	selectOptions?: Array<FormSelectOptionProps | string>;
-	disableSelect?: boolean;
-	multipleSelect?: boolean;
-	center?: boolean;
-	onClick?: React.MouseEventHandler<HTMLSelectElement>;
+    selectOptions?: Array<FormSelectOptionProps | string>;
+    disableSelect?: boolean;
+    multipleSelect?: boolean;
+    center?: boolean;
+    onClick?: React.MouseEventHandler<HTMLSelectElement>;
 };
 
 export interface FormSelectProps extends SharedInputProps, FormSelectMetaProps {}
 
 const FormSelect: FC<FormSelectProps> = (props) => {
-	return (
-		<SharedElement {...props}>
-			<div style={selectDiv}>
-				<select
-					id={props.id}
-					style={{ ...selectStyles, ...getVariantCSS(props.variant || 'light') }}
-					disabled={!!props.disableSelect}
-					multiple={!!props.multipleSelect}
-					value={
-						typeof props.value === 'boolean' || props.value instanceof File
-							? ''
-							: props.value
-					}
-					onChange={props.onChange}
-					onClick={props.onClick}
-				>
-					{props.selectOptions?.map((option, index) => {
-						const { value, displayValue } =
-							typeof option === 'string'
-								? { value: option, displayValue: '' }
-								: { value: option.value, displayValue: option.displayValue };
-						return (
-							<option key={index} value={value}>
-								{displayValue || capitalize(value)}
-							</option>
-						);
-					})}
-				</select>
-			</div>
-		</SharedElement>
-	);
+    return (
+        <SharedElement {...props}>
+            <div style={selectDiv}>
+                <select
+                    id={props.id}
+                    style={{ ...selectStyles, ...getVariantCSS(props.variant || 'light') }}
+                    disabled={!!props.disableSelect}
+                    multiple={!!props.multipleSelect}
+                    value={
+                        typeof props.value === 'boolean' || props.value instanceof File
+                            ? ''
+                            : props.value
+                    }
+                    onChange={props.onChange}
+                    onClick={props.onClick}
+                >
+                    {props.selectOptions?.map((option, index) => {
+                        const { value, displayValue } =
+                            typeof option === 'string'
+                                ? { value: option, displayValue: '' }
+                                : { value: option.value, displayValue: option.displayValue };
+                        return (
+                            <option key={index} value={value}>
+                                {displayValue || capitalize(value)}
+                            </option>
+                        );
+                    })}
+                </select>
+            </div>
+        </SharedElement>
+    );
 };
 
 export default FormSelect;
