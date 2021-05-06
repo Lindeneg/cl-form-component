552db0da40ccea21dfbe7564e8d22d6dd6308dbd
Christian <hi@lindeneg.org>
2021-05-06 02:12:59 +0200

    better type support

--- src/components/Form/Form.util.ts
+++ src/components/Form/Form.util.ts
@@ -22,21 +22,27 @@ interface Input
         FormSelectMetaProps,
         FormTextFieldMetaProps {}
 
-interface Entry<T extends FormEntryConstraint> extends Input {
+interface Entry<T extends FormEntryConstraint, K extends FormValueType>
+    extends Omit<Input, 'value'> {
+    value?: K;
     elementType?: 'input' | 'text-field' | 'selection' | 'image';
-    options?: GetInputOptions<FormValueType, T>;
+    options?: GetInputOptions<K, T>;
 }
 
-export type Entries<T extends FormEntryConstraint> = { [K in keyof T]: Entry<T> };
+export type Entries<T extends FormEntryConstraint> = {
+    [K in keyof T]: Entry<T, T[K] extends File ? T[K] | null : T[K]>;
+};
 
-export type SubmissionResult<T extends FormEntryConstraint> = { [K in keyof T]: T[K] };
+export type SubmissionResult<T extends FormEntryConstraint> = {
+    [K in keyof T]: T[K] extends File ? T[K] | null : T[K];
+};
 
 export function getFormInputs<T extends FormEntryConstraint>(entries: Entries<T>): Inputs<T> {
     const inputs: Record<string, unknown> = {};
     Object.keys(entries).forEach((key) => {
-        const entry = entries[key];
-        let options = { ...entry.options };
+        const entry = entries[key] as Entry<FormEntryConstraint, FormValueType>;
         let value = entry.value || '';
+        let options = { ...entry.options };
         if (
             entry.elementType === 'selection' &&
             typeof entry.selectOptions !== 'undefined' &&
