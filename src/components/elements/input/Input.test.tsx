1f350e6bc482fa06b3c713b8a0f6d60fc38f8339
lindeneg <christian@lindeneg.org>
2021-09-22 04:44:04 +0200

    tests

--- src/components/elements/input/Input.test.tsx
+++ src/components/elements/input/Input.test.tsx
@@ -1,3 +1,82 @@
-import { Input } from "./Input";
+import React from "react";
+import userEvent from "@testing-library/user-event";
+import { render, queryByAttribute } from "@testing-library/react";
+import { Input, InputProps } from ".";
 
-test("", () => {});
+function getInput(
+  value = "",
+  props: Partial<InputProps> = {},
+  onChange: () => void = () => {}
+) {
+  return (
+    <Input {...props} id="some-id" value={value} onInputChange={onChange} />
+  );
+}
+
+describe("Input Test Suite", () => {
+  test("renders input", () => {
+    const { container } = render(getInput());
+    const target = queryByAttribute("id", container, "some-id");
+
+    expect(target).toBeInTheDocument();
+    expect(target?.getAttribute("type")).toBe("text");
+    expect(target).toHaveClass("MuiInputBase-input");
+  });
+
+  test("renders input with label", () => {
+    const { getByText } = render(getInput("", { label: "Testing Input" }));
+    const target = getByText("Testing Input");
+
+    expect(target).toBeInTheDocument();
+    expect(target.tagName).toBe("LABEL");
+    expect(target).toHaveClass("MuiFormLabel-root");
+  });
+
+  test("renders input with helper text", () => {
+    const { getByText } = render(
+      getInput("", { helperEl: "Please write something" })
+    );
+    const target = getByText("Please write something");
+
+    expect(target).toBeInTheDocument();
+    expect(target.tagName).toBe("P");
+    expect(target).toHaveClass("MuiFormHelperText-root");
+  });
+
+  test("renders input with error text", () => {
+    const { getByText } = render(
+      getInput("", { errorEl: "An error occurred", error: true })
+    );
+    const target = getByText("An error occurred");
+
+    expect(target).toBeInTheDocument();
+    expect(target.tagName).toBe("P");
+    expect(target).toHaveClass("Mui-error");
+  });
+
+  test("renders input with valid element", () => {
+    const { getByText } = render(
+      getInput("", {
+        validEl: <span style={{ color: "#0ca60c" }}>Looks great!</span>,
+        valid: true,
+      })
+    );
+    const target = getByText("Looks great!");
+
+    expect(target).toBeInTheDocument();
+    expect(target.tagName).toBe("SPAN");
+    expect(target).toHaveStyle({ color: "#0ca60c" });
+  });
+
+  test("handles input on change event", () => {
+    const mockOnChange = jest.fn();
+    const { container, rerender } = render(getInput("", {}, mockOnChange));
+    const target = queryByAttribute("id", container, "some-id");
+
+    expect(target?.getAttribute("value")).toBe("");
+    target && userEvent.type(target, "something");
+    rerender(getInput("something"));
+    expect(mockOnChange).toHaveBeenCalledTimes(9);
+    expect(target?.getAttribute("value")).toBe("something");
+  });
+});
--- src/components/elements/radio/Radio.test.tsx
+++ src/components/elements/radio/Radio.test.tsx
@@ -1,3 +1,100 @@
-import { Radio } from "./Radio";
+import React from "react";
+import userEvent from "@testing-library/user-event";
+import { render, queryByAttribute } from "@testing-library/react";
+import { Radio, RadioProps } from ".";
 
-test("", () => {});
+enum Selected {
+  ON = "ON",
+  OFF = "OFF",
+}
+
+function getRadio(
+  selected = Selected.OFF,
+  props: Partial<RadioProps> = {},
+  onChange: () => void = () => {}
+) {
+  return (
+    <Radio
+      data={[
+        {
+          id: "some-id",
+          name: "Some option",
+          value: Selected.ON,
+        },
+      ]}
+      {...props}
+      selectedValue={selected}
+      onRadioChange={onChange}
+    />
+  );
+}
+
+describe("Radio Test Suite", () => {
+  test("renders radio", () => {
+    const { container, getByText } = render(getRadio());
+    const target = queryByAttribute("id", container, "some-id");
+
+    expect(target).toBeInTheDocument();
+    expect(target?.getAttribute("type")).toBe("radio");
+    expect(target?.getAttribute("value")).toBe(Selected.ON);
+    expect(getByText("Some option")).toBeInTheDocument();
+  });
+
+  test("renders radio with label", () => {
+    const { getByText } = render(
+      getRadio(Selected.OFF, { label: "Testing Radio" })
+    );
+    const target = getByText("Testing Radio");
+
+    expect(target).toBeInTheDocument();
+    expect(target.tagName).toBe("LABEL");
+    expect(target).toHaveClass("MuiFormLabel-root");
+  });
+
+  test("renders radio with helper text", () => {
+    const { getByText } = render(
+      getRadio(Selected.OFF, { helperEl: "Please click this radio" })
+    );
+    const target = getByText("Please click this radio");
+
+    expect(target).toBeInTheDocument();
+    expect(target.tagName).toBe("P");
+    expect(target).toHaveClass("MuiFormHelperText-root");
+  });
+
+  test("renders radio with error text", () => {
+    const { getByText } = render(
+      getRadio(Selected.OFF, { errorEl: "An error occurred", error: true })
+    );
+    const target = getByText("An error occurred");
+
+    expect(target).toBeInTheDocument();
+    expect(target.tagName).toBe("P");
+    expect(target).toHaveClass("Mui-error");
+  });
+
+  test("renders radio with valid element", () => {
+    const { getByText } = render(
+      getRadio(Selected.OFF, {
+        validEl: <span style={{ color: "#0ca60c" }}>Looks great!</span>,
+        valid: true,
+      })
+    );
+    const target = getByText("Looks great!");
+
+    expect(target).toBeInTheDocument();
+    expect(target.tagName).toBe("SPAN");
+    expect(target).toHaveStyle({ color: "#0ca60c" });
+  });
+
+  test("handles radio on change event", () => {
+    const mockOnChange = jest.fn();
+    const { container, rerender } = render(
+      getRadio(Selected.OFF, {}, mockOnChange)
+    );
+    const target = queryByAttribute("id", container, "some-id");
+    target && userEvent.click(target);
+    rerender(getRadio(Selected.ON));
+    expect(mockOnChange).toHaveBeenCalledTimes(1);
+  });
+});
