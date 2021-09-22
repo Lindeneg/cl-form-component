2d98bbfb7c14133fb58aa9907dccf35f8f0c06ad
lindeneg <christian@lindeneg.org>
2021-09-22 04:43:46 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-export { Input } from "./components/elements/input/Input";
-export { Checkbox } from "./components/elements/checkbox/Checkbox";
-export type { InputProps } from "./components/elements/input/Input";
-export type { CheckboxProps } from "./components/elements/checkbox/Checkbox";
+export { Input } from "./components/elements/input";
+export { Radio } from "./components/elements/radio";
+export { Checkbox } from "./components/elements/checkbox";
+export { Select } from "./components/elements/select";
+export type { InputProps } from "./components/elements/input";
+export type { RadioProps, MetaRadioProps } from "./components/elements/radio";
+export type {
+  CheckboxProps,
+  MetaCheckboxProps,
+} from "./components/elements/checkbox";
+export type {
+  SelectProps,
+  SelectEntryProps,
+  SelectMetaEntry,
+  SelectTypeConstraint,
+} from "./components/elements/select";
