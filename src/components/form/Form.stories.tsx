17a9872818eaee11816a94a8d102d1831c2f2287
lindeneg <christian@lindeneg.org>
2021-09-25 00:46:10 +0200

    test stories

--- src/components/form/Form.stories.tsx
+++ src/components/form/Form.stories.tsx
@@ -1,11 +1,29 @@
 import React from "react";
-import { Form } from ".";
+import { Form, FormProps } from ".";
 
 export default {
   title: "Form",
   component: Form,
 };
 
+type Inputs = {
+  test: {
+    type: number;
+    element: "input";
+  };
+};
+
 export function SimpleForm({ ...args }: {}) {
-  return <Form {...args} />;
+  return (
+    <Form<Inputs>
+      {...args}
+      entries={{
+        test: {
+          helperEl: <p>Hello</p>,
+          options: { customRule: (v, s) => s.inputs.test.value === v },
+          onInputChange: () => null,
+        },
+      }}
+    />
+  );
 }
