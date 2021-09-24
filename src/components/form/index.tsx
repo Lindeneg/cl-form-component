c3914af8eb7084440a6c9499b5b87aae23912b19
lindeneg <christian@lindeneg.org>
2021-09-24 18:49:06 +0200

    begin on form component

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,5 +1,76 @@
-import { useForm } from "cl-use-form-state";
+import {
+  useForm,
+  FormEntryConstraint as ClFormEntryConstraint,
+  InputValueType,
+  Inputs,
+  GetInputOptions,
+} from "cl-use-form-state";
+import { Checkbox, CheckboxProps } from "../checkbox";
+import { Input, InputProps } from "../input";
+import { Radio, RadioProps } from "../radio";
+import { Select, SelectProps, SelectTypeConstraint } from "../select";
+import { Switch, SwitchProps } from "../switch";
 
-export function Form() {
-  return <div></div>;
+export type FormElementConstraint =
+  | "checkbox"
+  | "input"
+  | "radio"
+  | "select"
+  | "switch";
+
+export type FormEntryConstraint = {
+  [key: string]: { type: InputValueType; element: FormElementConstraint };
+};
+
+export type FormElementProps<T extends FormElementConstraint> = Omit<
+  T extends "checkbox"
+    ? CheckboxProps
+    : T extends "input"
+    ? InputProps
+    : T extends "radio"
+    ? RadioProps
+    : T extends "select"
+    ? SelectProps<SelectTypeConstraint>
+    : T extends "switch"
+    ? SwitchProps
+    : Record<string, unknown>,
+  "value"
+>;
+
+export type FormElement<
+  T extends ClFormEntryConstraint,
+  K extends InputValueType,
+  P extends FormElementConstraint
+> = {
+  value: K;
+  options?: GetInputOptions<K, T>;
+} & FormElementProps<P>;
+
+export type Entries<T extends FormEntryConstraint> = {
+  [K in keyof T]: FormElement<
+    { T: T[K]["type"] },
+    T[K]["type"],
+    T[K]["element"]
+  >;
+};
+
+export type FormProps<T extends FormEntryConstraint> = {
+  data: Entries<T>;
+  wrapperClass?: string;
+  wrapperStyle?: React.CSSProperties;
+  formClass?: string;
+  formStyle?: React.CSSProperties;
+};
+
+export function Form<T extends FormEntryConstraint>({
+  wrapperClass,
+  wrapperStyle,
+  formClass,
+  formStyle,
+}: FormProps<T>) {
+  return (
+    <div className={wrapperClass} style={wrapperStyle}>
+      <form className={formClass} style={formStyle}></form>
+    </div>
+  );
 }
